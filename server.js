fc97a1fbad3f831d39a2a2ab148dfe7584033c2e
DonovanDwyer <dwyer.donovan@gmail.com>
2019-05-04 17:05:24 -0400

    socket server fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = require('http').createServer(app);
 
 app.use(express.static(path.join(__dirname, '/build')));
-app.get('/*', (req, res, next) => res.sendFile(__dirname + './index.html'));
+app.get('/', (req, res, next) => res.sendFile(__dirname + './index.html'));
 
 const io = socketIO(server);
 
